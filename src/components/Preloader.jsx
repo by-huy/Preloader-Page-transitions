89a5e5fc44148970a52709682e7a9c1aed3d2f46
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-09-04 11:25:59 +1000

    formatting

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,25 +1,31 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 export default function Preloader() {
-    const slideUp = {
-        initial: {
-            y: 0
-        },
-        exit: {
-            y: "-100vh",
-            transition: {duration: 1, ease: [0.83, 0, 0.17, 1]}
-        }
-    }
+  const slideUp = {
+    initial: {
+      y: 0,
+    },
+    exit: {
+      y: "-100vh",
+      transition: { duration: 1, ease: [0.83, 0, 0.17, 1] },
+    },
+  };
 
-    
-
-   
-
-    return (
-        <motion.div variants={slideUp} initial="initial" exit="exit" className="h-screen w-screen z-[999] fixed top-0 left-0 bg-white flex justify-center items-center">
-            <motion.p className="font-bold tracking-tight" transition={{duration: 1, ease: [0.83, 0, 0.17, 1]}} initial={{opacity: 0, y: 10}} animate={{opacity: 1, y:0 }}>
-                ContemptStudios
-            </motion.p>
-        </motion.div>
-    )
+  return (
+    <motion.div
+      variants={slideUp}
+      initial="initial"
+      exit="exit"
+      className="h-screen w-screen z-[999] fixed top-0 left-0 bg-white flex justify-center items-center"
+    >
+      <motion.p
+        className="font-bold tracking-tight"
+        transition={{ duration: 1, ease: [0.83, 0, 0.17, 1] }}
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        ContemptStudios
+      </motion.p>
+    </motion.div>
+  );
 }
